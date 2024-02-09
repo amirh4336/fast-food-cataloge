6cc7f3bb11fc132546cbe7ab08682016164dd057
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-02-09 17:34:34 +0330

    feat: add searchbar

--- src/CategoryList/categoryList.tsx
+++ src/CategoryList/categoryList.tsx
@@ -1,4 +1,5 @@
 import Loading from "../Loading/loading";
+import SearchBar from "../SearchBar/searchBar";
 // @ts-expect-error some bugs in get data from axios type
 import customAxios from "../axios";
 import { FC, useEffect, useState } from "react";
@@ -29,24 +30,27 @@ const CategoryList: FC<ICategoryListProps> = ({ filterItems }) => {
     }
 
     return (
-      <ul className="nav">
-        <li className="nav-item" onClick={() => filterItems()}>
-          <a className="nav-link" href="#">
-            همه فست فود ها
-          </a>
-        </li>
-        {categories.map((category: { id: number; name: string }) => (
-          <li
-            className="nav-item"
-            key={category.id}
-            onClick={() => filterItems(String(category.id))}
-          >
+      <div className="ps-3 w-100 d-flex align-items-center justify-content-between gap-5">
+        <ul className="nav">
+          <li className="nav-item" onClick={() => filterItems()}>
             <a className="nav-link" href="#">
-              {category.name}
+              همه فست فود ها
             </a>
           </li>
-        ))}
-      </ul>
+          {categories.map((category: { id: number; name: string }) => (
+            <li
+              className="nav-item"
+              key={category.id}
+              onClick={() => filterItems(String(category.id))}
+            >
+              <a className="nav-link" href="#">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <SearchBar />
+      </div>
     );
   };
 
