820be35ad5b3f50024989f6a0288ee3ae73c06a8
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-02-09 12:58:26 +0330

    feat: add fastFoodItem

--- src/FastFoodList/fastFoodList.tsx
+++ src/FastFoodList/fastFoodList.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
 import { IFoodItem } from "../data";
+import FastFoodItem from "../FastFoodItem/fastFoodItem";
 
 interface IFastFoodListProps {
   fastFoodItems : IFoodItem[]
 }
 
 const FastFoodList :FC<IFastFoodListProps> = ({fastFoodItems}) => {
-  return <div>FastFoodList</div>;
+  return <div className="row">
+    {fastFoodItems.map(fastFood => (
+      <div className="col-md-4 col-sm-6 mb-grid-gutter" key={fastFood.id}>
+        <FastFoodItem {...fastFood} />
+      </div>
+    )) }
+  </div>;
 };
 
 export default FastFoodList;
