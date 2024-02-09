0ba20e20b289b1bea3eaa2e0b780d6530789a499
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-02-09 13:20:48 +0330

    feat: placeHoder for iamge in fastfoodItem

--- src/FastFoodItem/fastFoodItem.tsx
+++ src/FastFoodItem/fastFoodItem.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { IFoodItem } from "../data";
 import { HiShoppingCart } from "react-icons/hi";
 
-
+import "./fastFoodItem.css"
 
 const FastFoodItem: FC<IFoodItem> = ({
   name,
@@ -15,12 +15,14 @@ const FastFoodItem: FC<IFoodItem> = ({
       <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
         قیمت : {price.toLocaleString()} تومان
       </span>
-      <img className="card-img-top" src={imageUrl} />
+      <div className="card__placeholder">
+        <img className="card-img-top" src={imageUrl} />
+      </div>
       <div className="card-body text-center pt-3 pb-4 d-flex flex-column">
         <h5 className="mb-2">{name}</h5>
         <div className="fs-ms fw-bold text-muted mb-3">{ingredients}</div>
-        <button className="btn btn-outline-success btn-sm w-100 mt-auto fw-bold" >
-        <HiShoppingCart className="fs-5 ms-3" />
+        <button className="btn btn-outline-success btn-sm w-100 mt-auto fw-bold">
+          <HiShoppingCart className="fs-5 ms-3" />
           افزودن به سبد خرید
         </button>
       </div>
